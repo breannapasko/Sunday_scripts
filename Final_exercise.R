884e8ff76df1a6b69e9e799d345455acfc4c69eb
breannapasko <breannapasko@gmail.com>
2015-04-19 16:12:27 -0500

    Calculated mean, max, and min Life Expectancies

--- Final_exercise.R
+++ Final_exercise.R
@@ -12,8 +12,21 @@ gap.in %>%
 gap.in %>%
   filter(country == "Oman") -> Oman
 
-
-
+#Made 3 plots for exercise
 plot(x=Brazil$year,y=Brazil$gdpPercap,main="Brazil",xlab="year",ylab="GDP")
 plot(x=Djibouti$year,y=Djibouti$gdpPercap,main="Djibouti",xlab="year",ylab="GDP")
 plot(x=Oman$year,y=Oman$gdpPercap,main="Oman",xlab="year",ylab="GDP")
+
+#Calculate mean,min, and max life expectancy for each continent
+
+mean(Brazil$lifeExp)
+min(Brazil$lifeExp)
+max(Brazil$lifeExp)
+
+mean(Djibouti$lifeExp)
+min(Djibouti$lifeExp)
+max(Djibouti$lifeExp)
+
+mean(Oman$lifeExp)
+min(Oman$lifeExp)
+max(Oman$lifeExp)
