47e768fafd828e4c5e089766dc0672ffcbb272dd
breannapasko <breannapasko@gmail.com>
2015-04-19 13:35:59 -0500

    Add line to read in data

--- Sunday_pm_gapminder.R
+++ Sunday_pm_gapminder.R
@@ -1,3 +1,6 @@
 #This is a script to explore the gapminder dataset using R
 #Breanna Pasko breannapasko@gmail.com
 #2015-19-04
+
+#Read in data as tab-delimited
+gap.in<- read.table("output/combined_gapMinder.tsv", sep = "\t")
